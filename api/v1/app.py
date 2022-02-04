f6a3fd1c6cf9ac2722d7b5f5c7d7d8d12b9f5ea1
JuanDavidPena <j_david0915@hotmail.com>
2022-02-04 13:28:46 -0500

    [modify]- index and app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,15 +2,15 @@
 """
 returnS the status of the API
 """
-from flask import Flask
-from flask.json import jsonify
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+CORS(app, resources={r"/*": {"origins": ["0.0.0.0"]}})
 
 @app.teardown_appcontext
 def close_session():
@@ -25,10 +25,6 @@ def error_404():
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST')
-    port = os.getenv('HBNB_API_PORT')
-    if host is None:
-        host = "0.0.0.0"
-    if port is None:
-        port = '5000'
-    app.run(threaded=True)
+    app_host = os.getenv("HBNB_API_HOST", default="0.0.0.0")
+    app_port = os.getenv("HBNB_API_PORT", default=5000)
+    app.run(host=app_host, port=int(app_port))
